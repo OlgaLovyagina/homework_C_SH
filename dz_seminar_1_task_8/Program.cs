0ea8e79e866bed33e969ccc1ea4e3194279aee37
Olga <ol_0524@mail.ru>
2022-09-18 19:49:49 +0300

    Решение задача 8

---
+++ dz_seminar_1_task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N > 0), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число , которое больше 0 : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while(a <= num)
+{
+if(a % 2 ==0)
+{
+    Console.Write(a + "  ");    
+    a = a + 1;
+}
+else 
+{
+    a = a + 1;    
+}
+ }
+ Console.WriteLine();
